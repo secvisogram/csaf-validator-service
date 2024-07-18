5a67d9a876d220df625f80b64ac822c09f8b60bb
Marco Korinth <marco@bizfactory.tech>
2024-07-18 14:43:36 +0200

    chore: update dependencies

--- backend/lib/app.js
+++ backend/lib/app.js
@@ -10,7 +10,7 @@ export default async function(fastify) {
     openapi: {
       info: {
         title: 'CSAF Validator Service',
-        version: '1.3.34',
+        version: '1.3.35',
         description:
           'This is a service to validate documents against the CSAF standard.',
         contact: {
