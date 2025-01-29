76129d2baf25b4f1155b2329cf1b61989f027d7e
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2025-01-29 09:02:04 +0100

    chore: update dependencies

--- backend/lib/app.js
+++ backend/lib/app.js
@@ -10,7 +10,7 @@ export default async function (fastify) {
     openapi: {
       info: {
         title: 'CSAF Validator Service',
-        version: '1.3.44',
+        version: '1.3.45',
         description:
           'This is a service to validate documents against the CSAF standard.',
         contact: {
