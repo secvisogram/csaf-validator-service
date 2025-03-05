44dd82af513512ed7df74f40fa526a9fd413e656
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2025-03-05 12:37:36 +0100

    refactor: clean up version replace script

--- scripts/replaceVersionInFiles.js
+++ scripts/replaceVersionInFiles.js
@@ -4,83 +4,74 @@
  * - backend/lib/app.js
  */
 
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { EOL } from 'node:os'
+import { readFile, writeFile } from 'node:fs/promises'
 
 // __dirname is not available in ES modules, so we construct it.
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = dirname(__filename)
 
+/**
+ * @param {string} newVersion
+ */
 async function replaceVersionInAsciidoc(newVersion) {
-  const filePath = path.join(
+  const filePath = resolve(
     __dirname,
-    '..',
-    'documents',
-    'generated',
-    'asciidoc',
-    'index.adoc'
+    '../documents/generated/asciidoc/index.adoc'
   )
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Error reading the file:', err)
-    }
+  const data = await readFile(filePath, 'utf8')
 
-    // Split the file into lines
-    const lines = data.split(EOL)
+  // Split the file into lines
+  const lines = data.split(EOL)
 
-    // Check if there are at least 3 lines
-    if (lines.length >= 3) {
-      // Replace the content on line 3 (index 2)
-      lines[2] = newVersion
+  // Check if there are at least 3 lines
+  if (lines.length >= 3) {
+    // Replace the content on line 3 (index 2)
+    lines[2] = newVersion
 
-      // Join the lines back together
-      const updatedData = lines.join('\n')
+    // Join the lines back together
+    const updatedData = lines.join('\n')
 
-      // Write the updated data back to the file
-      fs.writeFile(filePath, updatedData, 'utf8', (err) => {
-        if (err) {
-          throw new Error('Error writing the file:', err)
-        }
-      })
-    }
-  })
+    // Write the updated data back to the file
+    await writeFile(filePath, updatedData, 'utf8')
+  }
 }
 
+/**
+ * @param {string} newVersion
+ */
 async function replaceVersionInHtml(newVersion) {
-  const filePath = path.join(
-    __dirname,
-    '..',
-    'documents',
-    'generated',
-    'html',
-    'index.html'
-  )
+  const filePath = resolve(__dirname, '../documents/generated/html/index.html')
 
-  let fileContent = fs.readFileSync(filePath, 'utf8')
+  let fileContent = await readFile(filePath, 'utf8')
   fileContent = fileContent.replace(
     /(>Version:\s*)\d.\d.\d.(<)/,
     `$1${newVersion}$2`
   )
   // Write the updated content back to the file.
-  fs.writeFileSync(filePath, fileContent, 'utf8')
+  await writeFile(filePath, fileContent, 'utf8')
 }
 
+/**
+ * @param {string} newVersion
+ */
 async function replaceVersionInAppJs(newVersion) {
-  const filePath = path.join(__dirname, '..', 'backend', 'lib', 'app.js')
+  const filePath = resolve(__dirname, '../backend/lib/app.js')
 
-  let fileContent = fs.readFileSync(filePath, 'utf8')
+  let fileContent = await readFile(filePath, 'utf8')
   fileContent = fileContent.replace(
     /(\s*version:\s*')[^']+(')/,
     `$1${newVersion}$2`
   )
   // Write the updated content back to the file.
-  fs.writeFileSync(filePath, fileContent, 'utf8')
+  await writeFile(filePath, fileContent, 'utf8')
 }
 
-// Import the package.json file using an import assertion (Node 17+ with "type": "module")
-import pkg from '../package.json' assert { type: 'json' }
+const pkg = /** @type {import('../package.json')} */ (
+  JSON.parse(await readFile(resolve(__dirname, '../package.json'), 'utf-8'))
+)
 const newVersion = pkg.version
 
 replaceVersionInAsciidoc(newVersion)
