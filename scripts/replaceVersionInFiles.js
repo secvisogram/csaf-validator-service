d94823e3409aadf881a53cbc19fdc9f4bb43dc72
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2025-03-12 14:47:35 +0100

    chore: make version update process more robust

--- scripts/replaceVersionInFiles.js
+++ scripts/replaceVersionInFiles.js
@@ -4,76 +4,73 @@
  * - backend/lib/app.js
  */
 
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { EOL } from 'node:os'
+import path, { resolve } from 'node:path'
 import { readFile, writeFile } from 'node:fs/promises'
+import { exec, fork } from 'node:child_process'
+import waitOn from 'wait-on'
+import { promisify } from 'node:util'
+import { openApiInfo } from '../backend/lib/openApiInfo.js'
+import prettier from 'prettier'
 
-// __dirname is not available in ES modules, so we construct it.
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-/**
- * @param {string} newVersion
- */
-async function replaceVersionInAsciidoc(newVersion) {
-  const filePath = resolve(
-    __dirname,
-    '../documents/generated/asciidoc/index.adoc'
-  )
-  const data = await readFile(filePath, 'utf8')
-
-  // Split the file into lines
-  const lines = data.split(EOL)
-
-  // Check if there are at least 3 lines
-  if (lines.length >= 3) {
-    // Replace the content on line 3 (index 2)
-    lines[2] = newVersion
-
-    // Join the lines back together
-    const updatedData = lines.join('\n')
-
-    // Write the updated data back to the file
-    await writeFile(filePath, updatedData, 'utf8')
-  }
-}
+const execAsync = promisify(exec)
 
 /**
- * @param {string} newVersion
+ * During the run of the script the dev server is started to update
+ * the api documentation. This is the port which is used by the server.
  */
-async function replaceVersionInHtml(newVersion) {
-  const filePath = resolve(__dirname, '../documents/generated/html/index.html')
-
-  let fileContent = await readFile(filePath, 'utf8')
-  fileContent = fileContent.replace(
-    /(>Version:\s*)\d.\d.\d.(<)/,
-    `$1${newVersion}$2`
-  )
-  // Write the updated content back to the file.
-  await writeFile(filePath, fileContent, 'utf8')
-}
-
-/**
- * @param {string} newVersion
- */
-async function replaceVersionInAppJs(newVersion) {
-  const filePath = resolve(__dirname, '../backend/lib/app.js')
-
-  let fileContent = await readFile(filePath, 'utf8')
-  fileContent = fileContent.replace(
-    /(\s*version:\s*')[^']+(')/,
-    `$1${newVersion}$2`
-  )
-  // Write the updated content back to the file.
-  await writeFile(filePath, fileContent, 'utf8')
-}
+const SERVER_PORT = 8082
+const OPEN_API_INFO_MODULE = resolve(
+  import.meta.dirname,
+  '../backend/lib/openApiInfo.js'
+)
 
 const pkg = /** @type {import('../package.json')} */ (
-  JSON.parse(await readFile(resolve(__dirname, '../package.json'), 'utf-8'))
+  JSON.parse(
+    await readFile(resolve(import.meta.dirname, '../package.json'), 'utf-8')
+  )
 )
 const newVersion = pkg.version
 
-replaceVersionInAsciidoc(newVersion)
-replaceVersionInHtml(newVersion)
-replaceVersionInAppJs(newVersion)
+/**
+ * The version of the package is duplicated in the backend/lib/openApiInfo.js file
+ * since the app cannot access the outer package.json when run in a docker container.
+ * Here we synchronize that version string with the package.json.
+ */
+await writeFile(
+  OPEN_API_INFO_MODULE,
+  prettier.format(
+    `export const openApiInfo = /** @type {const} */ (${JSON.stringify({
+      ...openApiInfo,
+      version: newVersion,
+    })})`,
+    {
+      ...(await prettier.resolveConfig(OPEN_API_INFO_MODULE)),
+      filepath: OPEN_API_INFO_MODULE,
+    }
+  )
+)
+
+/**
+ * Spawn the server process and capture a handle `srv` to its child process
+ * to be able to kill the process at the end of the script.
+ *
+ * @type {import('node:child_process').ChildProcess}
+ */
+const srv = await new Promise((resolve) => {
+  const srv = fork('./server.js', {
+    cwd: path.resolve(import.meta.dirname, '../backend'),
+  })
+  resolve(
+    waitOn({
+      resources: [`tcp:${SERVER_PORT}`],
+    }).then(() => srv)
+  )
+})
+
+await execAsync(
+  `npx openapi-generator-cli generate -i http://localhost:${SERVER_PORT}/docs/json -g html -o ./documents/generated/html/`
+)
+await execAsync(
+  `openapi-generator-cli generate -i http://localhost:${SERVER_PORT}/docs/json -g asciidoc -o ./documents/generated/asciidoc/`
+)
+srv.kill('SIGTERM')
