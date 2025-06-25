aec4a8b75f79cf2b5cb493cc321ff421773cd72b
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-06-25 09:03:27 +0200

    2.0.4

--- backend/lib/openApiInfo.js
+++ backend/lib/openApiInfo.js
@@ -1,6 +1,6 @@
 export const openApiInfo = /** @type {const} */ ({
   title: 'CSAF Validator Service',
-  version: '2.0.3',
+  version: '2.0.4',
   description:
     'This is a service to validate documents against the CSAF standard.',
   contact: {
