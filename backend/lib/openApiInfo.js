ad97dec068a6616e0276cdd4e1183ca2fffa191f
bendo-eXX <benjamin.dorn@exxcellent.de>
2025-10-08 09:13:41 +0200

    2.0.12

--- backend/lib/openApiInfo.js
+++ backend/lib/openApiInfo.js
@@ -1,6 +1,6 @@
 export const openApiInfo = /** @type {const} */ ({
   title: 'CSAF Validator Service',
-  version: '2.0.11',
+  version: '2.0.12',
   description:
     'This is a service to validate documents against the CSAF standard.',
   contact: {
