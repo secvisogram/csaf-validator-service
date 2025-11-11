35a079d83768f877275c180f9d7e95c4dc034224
Tommy Lehmann <tlehmann@kunbus.com>
2025-11-11 12:05:25 +0100

    Add a pure nodejs http client to prevent adding curl to image
    Docker HEALTHCHECKS are executed from within the container. To support standard docker healthchecks there need to be a script that is able to check if the server is running correctly. To avoid further dependancies like curl and to be able to use a distroless base in the future the checktool is implemented in JavaScript and executed with node like the server itself. Using the same technology provides the possiblity load the same config file and thus make the healthcheck in sync with the server configuration in regards of pathes and ports.

---
+++ health_check/healthcheck.js
@@ -0,0 +1,33 @@
+import * as http from 'node:http'
+import config from 'config'
+
+const options = { hostname: 'localhost', port: config.get('port'), path: '/health', method: 'GET' };
+
+http
+  .request(options, (res) => {
+    let body = '';
+
+    res.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        const response = JSON.parse(body);
+        if (response.status === 'ok') {
+          process.exit(0);
+        }
+
+        console.log('Unhealthy response received: ', body);
+        process.exit(1);
+      } catch (err) {
+        console.log('Error parsing JSON response body: ', err);
+        process.exit(1);
+      }
+    });
+  })
+  .on('error', (err) => {
+    console.log('Error: ', err);
+    process.exit(1);
+  })
+  .end();
